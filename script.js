694b6645f8c7603e5fccf64ca893267324220e9f
Gautam kumar <gautamsiddharth2013@gmail.com>
2025-10-16 15:52:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function highlight() {
     //Write your code here
+	let over = document.querySelectorAll("strong");
+	over.forEach(element => {
+		element.style.color = "rgb(0, 128, 0)";
+	})
 
 
 }
@@ -7,6 +11,15 @@ function highlight() {
 
 function return_normal() {
     //Write your code here
+	let out = document.querySelectorAll("strong");
+	out.forEach(element => {
+		element.style.color = "rgb(0, 0, 0)";
+	})
 
-    
 }
+
+
+
+
+
+
